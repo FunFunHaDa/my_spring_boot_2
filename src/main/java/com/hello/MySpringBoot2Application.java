fbeaca0e3075e6083d17c175969425988baf27b8
funfunhada <kjhkjh0929@gmail.com>
2020-01-15 12:50:53 +0900

    initial commit

---
+++ src/main/java/com/hello/MySpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBoot2Application.class, args);
+	}
+
+}
